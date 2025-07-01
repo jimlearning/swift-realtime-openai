91bfc6a4e2abe1ad0636bfeb03d92e0bc40d862f
jimlearning <waz0820@gmail.com>
2025-07-01 14:46:22 +0800

    feat: separate package, supports webrtc and websocket separately

--- Package.swift
+++ Package.swift
@@ -3,22 +3,47 @@
 import PackageDescription
 
 let package = Package(
-	name: "OpenAIRealtime",
-	platforms: [
-		.iOS(.v17),
-		.tvOS(.v17),
-		.macOS(.v14),
-		.watchOS(.v10),
-		.visionOS(.v1),
-		.macCatalyst(.v17),
-	],
-	products: [
-		.library(name: "OpenAIRealtime", type: .static, targets: ["OpenAIRealtime"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
-	],
-	targets: [
-		.target(name: "OpenAIRealtime", dependencies: ["WebRTC"], path: "./src"),
-	]
+    name: "OpenAIRealtime",
+    platforms: [
+        .iOS(.v16),
+        .tvOS(.v16),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .visionOS(.v1),
+        .macCatalyst(.v16),
+    ],
+    products: [
+        .library(name: "OpenAIRealtime", type: .static, targets: ["OpenAIRealtime"]),
+        .library(name: "OpenAIRealtimeWebSocket", type: .static, targets: ["OpenAIRealtimeWebSocket"]),
+        .library(name: "OpenAIRealtimeWebRTC", type: .static, targets: ["OpenAIRealtimeWebRTC"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stasel/WebRTC.git", branch: "latest")
+    ],
+    targets: [
+        .target(
+            name: "OpenAIRealtime",
+            dependencies: [],
+            path: "./src",
+            exclude: [
+                "Connectors/WebSocketConnector.swift",
+                "Connectors/WebRTCConnector.swift",
+            ]
+        ),
+        .target(
+            name: "OpenAIRealtimeWebSocket",
+            dependencies: ["OpenAIRealtime"],
+            path: "./src/Connectors",
+            sources: ["WebSocketConnector.swift"]
+        ),
+        .target(
+            name: "OpenAIRealtimeWebRTC",
+            dependencies: [
+                "OpenAIRealtime",
+                .product(name: "WebRTC", package: "WebRTC"),
+            ],
+            path: "./src/Connectors",
+            sources: ["WebRTCConnector.swift"]
+        ),
+    ]
 )
